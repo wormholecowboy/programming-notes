042bc986d0dc77c0822e46d0dd40d86acad305e5
wormholecowboy <wormholecowboy@gmail.com>
2024-07-23 17:16:30 -0400

    bash

--- bash-scripting.sh
+++ bash-scripting.sh
@@ -260,9 +260,11 @@ i   case insensitive
 g   all line occurences
 1-9 pick the exact occurence
 w   creates a file of only changed items
-e   add a script to the commands to be executed 
+e   use multiple commands
+f   use a script file for sed commands
+d   delete
 
-## edit in place and create backup
+## edit in place and create backup (insensitive)
 sed -i.bak 'patterns' file.txt
 
 ## can use any delimiter
@@ -270,11 +272,16 @@ sed 's#serach#replace#gi' file.txt
 
 ## delete a specific line
 sed '/searchPatter/d' file.txt
+## delete blank lines
+sed '/^$/d' file
 
 ## combine multi commands with ;
 sed '/search/d ; otherSearch/d' file.txt
 OR
 sed -e 'command1' -e 'command2' file.txt
+
+## use a script
+sed -f script.sed file.txt
 
 ## replace only specific line
 sed '3 s/search/replace' file.txt
@@ -283,7 +290,7 @@ sed '3 s/search/replace' file.txt
 sed '1,32 s/search/replace' file.txt
 
 ## replace lines containing certain pattern
-### also works with range
+### also works with range; can use range from one regex to another
 sed '/containingPatter/ s/search/replace' file.txt
 
 
