da313da5fb234d8eb7281cd9cefbffbdfed0101c
wormholecowboy <wormholecowboy@gmail.com>
2024-09-18 17:52:51 -0400

    go

--- go.go
+++ go.go
@@ -2,10 +2,14 @@
 // Strings use double quotes or backticks
 // Return errors from fns is a go idiom
 // all data types are initialized to something, 0 or nil usually
-// arguments are pass by value (makes a copy), except slices, maps and channels
 
 // build tags. Need blank line after.
 //go:build prod || dev 
+
+
+// FUNCTIONS
+// arguments are pass by value (makes a copy), except slices, maps and channels
+// functions can have named return values
 
 package main
 
@@ -14,6 +18,8 @@ import (
   "errors"
   "strings"
   "sync"
+  ioAlias "io"
+  "github.com/go-chi/chi"  // use "go mod tidy" to install ext dep
 )
 
 
@@ -36,6 +42,33 @@ func testFunc(x, y int) (int, int, error) {  // 2nd paren are return vars with t
     return x + y, x * y, err
 }
 
+// function literals (anon fns)
+fmt.Println("Add + double two numbers: ",
+        func(a, b int) int {
+            return (a + b) * 2
+        }(10, 2)) // Called with args 10 and 2
+    // => Add + double two numbers: 24
+
+// variadic parameters
+func learnVariadicParams(myStrings ...any) { // ... accepts 0 or more params. any is an alias for interface{}
+    // Iterate each value of the variadic, which is a slice
+    for _, param := range myStrings {
+        fmt.Println("param:", param)
+    }
+    // Pass variadic value as a variadic parameter.
+    fmt.Println("params:", fmt.Sprintln(myStrings...))  // ... unpacks the myStrings slice
+}
+
+
+// ERROR HANDLING
+// ", ok" idiom used to tell if something worked or not.
+m := map[int]string{3: "three", 4: "four"}
+    if x, ok := m[1]; !ok { // ok will be false because 1 is not in the map.
+        fmt.Println("no one there")
+    } else {
+        fmt.Print(x) // x would be the value, if it were in the map.
+    }
+
 // SWITCH
 func thingish() {
   switch{
@@ -43,7 +76,7 @@ func thingish() {
     //do thing
     // break is implicit
   case false:
-    // do things
+    // do thing
   default:
     // nothing
   }
@@ -52,8 +85,20 @@ func thingish() {
   switch thing {
   case 0:
     // do thing
+  case 1,2,3:
+    // can have mutli matches per case
   default:
     // thing
+  }
+
+  // TYPE SWITCH
+  switch v := i.(type) {
+  case T:
+      // here v has type T
+  case S:
+      // here v has type S
+  default:
+      // no match; here v has the same type as i
   }
 }
 
@@ -65,7 +110,7 @@ fmt.Println(intArr[1:3]) // print indices 1 and 2
 
 var intArr2 [3]int32 = [3]int32{1,2,3} // literal initialization
 var intArr2 [3]int32 = [...]int32{1,2,3} // inferred
-intArr3 := [...]int32{1,2,3} // even more concise
+intArr3 := [...]int32{1,2,3} // even more concise, infers num of members
 
 
 // SLICES
@@ -76,7 +121,7 @@ var intSlice []int32 = []int32{1,2,3}  // omitting number in brackets gives you 
 var results = []string{}  // make an empty slice
 
 intSlice = append(intSlice, 4)  // returns a new array at new mem location
-intSlice = append(intSlice, intSlice2...)  // append multiple
+intSlice = append(intSlice, intSlice2...)  // append a literal or ref to other slice, ellipsis unpacks
 len(intSlice) // how many members
 cap(intSlice) // total capacity
 
@@ -89,10 +134,15 @@ var myMap map[string]uint8 = make(map[string]uint8)  // keys are strings and val
 var myMap2 = map[string]uint3{"Brian": 16, "Shannon": 15} // literal init
 myMap2["Brian"]
 
-// WARNING: maps will always return something, even if keys don't exist
+// WARNING: maps will always return something, even if keys don't exist; returns the zero value of the value type
 var age, ok = myMap2["Jagger"]  // maps return 2nd element (bool) that you can use for checks
 
 delete(myMap2, "Brian") // does not return anything
+
+// Check if a key is present in the map like this:
+    if val, ok := m["one"]; ok {
+        // Do something
+    }
 
 
 // FOR LOOPS
@@ -172,28 +222,45 @@ myEngine.name
 myEngine3 := struct{mpg int, gallons int}
 
 // methods for structs
-func (e gasEngine) gallonsLeft() uint8 {
+// In general, all methods on a given type should have either value or pointer receivers, but not a mixture of both
+func (e gasEngine) gallonsLeft() uint8 {  // first paren is called receiver. can take pointer or value.
   return e.gallons
 }
 myEngine.gallonsLeft()
 
+// must use a pointer if you want to mutate struct members
+func (e *gasEngine) changeGallons() uint8 {
+  // mutate gallons
+}
+
 
 // INTERFACE
 // can set this as param so passed in structs must adhere to it
+// think of them as a tuple that holds (value, type)
 type engine interface {
   milesLeft() uint8
 }
+
+// use empty interfaces for unknown yptes
+interface{}
+
+// type assertion
+// assert that the interface value i holds the concrete type T and assigns the underlying T value to the variable t.
+t := i.(T)
+// use a check to avoid a panic, returns value and type check
+t, ok := i.(T)
 
 
 // POINTERS
 // make sure to assign pointer to an address before ref
 var p *int32   // creates pointer, init to nil, no ref yet
 var p *int32 = new(int32)  // points to address with 0 inside
+s := make([]int, 20)  // creates memory for 20 ints
 fmt.Println(*p)  // get the value of the address that p is pointing to (dereferencing the pointer)
 *p = 10  // sets new value at address p is pointing to
 
 var i int32
-p = &i  // assign p the memory address of i
+p = &i  // assign p the memory address of i. &i is pointer essentially.
 *p = 1  // value of i is also changed
 
 // pass by ref or value
@@ -285,7 +352,7 @@ func doThing2() {
 }
 
 func process2(c chan int)  {
-  defer close(c)  // this tells any other process using this channel that we are done. Defer runs right at the end of fn.
+  defer close(c)  // this tells any other process using this channel that we are done. Defer list runs after parent fn returns.
   for i:=0; i<5; i++ {
     c <-i
   }
@@ -309,16 +376,77 @@ func process2(c chan int)  {
 
 // SELECT
 // like an if statement for channels
-
+// It selects a case at random out of the cases that are ready to communicate.
 func whatever(chan1 chan string, chan2 chan string) {
   select{  // listens for whatever channel updates first and runs that case
   case myVar := <-chan1:
     // do a thing
   case myVar := <-chan2:
     // do a thing
+  case <-chan2:
+    // discard a value
   }
 }
 
 
 // GENERICS
+func sumSlice[T int | float32](slice []T) T {  // creates a generic that only accepts int or float32
+  var sum T
+  for _, val := range slice {
+    sum += val
+  }
+  return sum
+}
+
+var intSlice = []int{1,2,3}
+sumSlice[int](intSlice)
+
+// can be used for structs too
+type car [T gasEngine | electricEngine]struct{
+  make string
+  engine T
+}
+
+
+// WEB, API, SERVER
+type pair struct {
+}
+
+func learnWebProgramming() {
+    // First parameter of ListenAndServe is TCP address to listen to.
+    // Second parameter is an interface, specifically http.Handler.
+    go func() {
+        err := http.ListenAndServe(":8080", pair{})
+        fmt.Println(err) // don't ignore errors
+    }()
+    requestServer()
+}
+
+// Make pair an http.Handler by implementing its only method, ServeHTTP.
+func (p pair) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    // Serve data with a method of http.ResponseWriter.
+    w.Write([]byte("You learned Go in Y minutes!"))
+}
+
+func requestServer() {
+    resp, err := http.Get("http://localhost:8080")
+    fmt.Println(err)
+    defer resp.Body.Close()
+    body, err := io.ReadAll(resp.Body)
+    fmt.Printf("\nWebserver said: `%s`", string(body))
+}
+
+
+// CLI
+go run main.go
+errcheck
+
+go build  // binary
+GOOS=windows go build  // os specific
+go get <githublink>  // install bin from this repo in your gopath
+go mod init //
+
+go doc fmt  // print docs for fmt
+go doc fmt Printf
+godoc -http :6060  // run a local docs server
 
