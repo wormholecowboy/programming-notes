b4550f24c638aef74ca3ec99d89cb7606985f873
wormholecowboy <wormholecowboy@gmail.com>
2024-10-11 12:32:34 -0400

    go

--- go.go
+++ go.go
@@ -18,6 +18,8 @@ import (
   "errors"
   "strings"
   "sync"
+  "os"
+  "bufio"
   ioAlias "io"
   "github.com/go-chi/chi"  // use "go mod tidy" to install ext dep
 )
@@ -233,6 +235,11 @@ func (e *gasEngine) changeGallons() uint8 {
   // mutate gallons
 }
 
+// struct embedding, nesting
+type someOtherGasEngine struct {
+  extraThingINeed string
+  ge gasEngine  // ge alias is optional. Without, its an anonymous struct and no need for ge or anything in dot syntax. Can access nested fields directly on someOtherGasEngine.
+}
 
 // INTERFACE
 // can set this as param so passed in structs must adhere to it
@@ -255,13 +262,20 @@ t, ok := i.(T)
 // make sure to assign pointer to an address before ref
 var p *int32   // creates pointer, init to nil, no ref yet
 var p *int32 = new(int32)  // points to address with 0 inside
+
 s := make([]int, 20)  // creates memory for 20 ints
 fmt.Println(*p)  // get the value of the address that p is pointing to (dereferencing the pointer)
 *p = 10  // sets new value at address p is pointing to
 
 var i int32
 p = &i  // assign p the memory address of i. &i is pointer essentially.
-*p = 1  // value of i is also changed
+*p = 1  // value of i is changed
+
+// no need to dereference like this when accessing struct fields
+var thing = someStruct{1,2}
+p = &thing
+p.x  // use this
+(*p).x // instead of this
 
 // pass by ref or value
 var thing1 = [3]float32{1,2,3}
@@ -374,6 +388,11 @@ func process2(c chan int)  {
   }
 }
 
+// can also make an slice of channels
+var chans = make([]chan int)
+chans[0] = make(chan int)
+
+
 // SELECT
 // like an if statement for channels
 // It selects a case at random out of the cases that are ready to communicate.
@@ -443,10 +462,78 @@ errcheck
 
 go build  // binary
 GOOS=windows go build  // os specific
-go get <githublink>  // install bin from this repo in your gopath
-go mod init //
 
+go get <githubLink>  // install bin from this repo in your gopath
+
+go mod init github.com/user/moduleName // init your project
+
+
+go help
 go doc fmt  // print docs for fmt
 go doc fmt Printf
 godoc -http :6060  // run a local docs server
+
+go mod tidy // install your exter dep in your imports
+
+
+// FMT
+fmt.Println()  // prints a line to console
+
+fmt.Printf("formatted val: %v", someVal)   // prints to console
+// %f float
+// %.1f  one decimal place
+// %t  true or false
+
+fmt.Sprint()  // returns a string
+fmt.Sprintf() 
+
+fmt.Printf(`
+  use backticks for multiline
+  `)
+
+
+
+// WRITING & READING
+os.WriteFile("filename.txt", []byte(stringToConvertToBytes), 0644)  // takes a byte slice. Last arg is Linux permissions
+data, err := os.ReadFile("filename.txt")
+
+
+// ERRORS
+func temp() int, error {
+  data, err := os.ReadFile("filename.txt")
+
+  if err != nil {
+    return errors.New("My custom error")
+    // or return nil, err
+    // or panic("Crashing the prog now...")
+  }
+}
+
+
+// PACKAGES
+// every go file must be part of a package
+// simply add a file to package main and its fns will be callable
+// cannot have different packages in same folder. create same folder named same as package.
+
+// to import, prepend module path, then 
+// ex: "github.com/wormhole/thingy/myPackageName"
+// fns must start with uppercase to be exported
+//   same for structs and their fields
+
+
+// USER INPUT
+fmt.Scan(&someVar) // can only be one word
+
+// longer string
+reader := bufio.NewReader(os.Stdin)
+text, err := reader.ReadString('\n') // break value
+text = strings.TrimSuffix(text, "\n") 
+text = strings.TrimSuffix(text, "\r")  // for windows
+
+
+
+
+
+
+
 
